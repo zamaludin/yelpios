b138baaab01cfc90fb984a782c4f83d15265f86a
zamaludin <zamaludin@gits.id>
2022-05-27 22:21:41 +0700

    Initial Commit

---
+++ testios42race/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testios42race
+//
+//  Created by Zamaludin Abdulah on 27/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
